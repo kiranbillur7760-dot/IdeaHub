913a0c582715e13e8553676bc734c4c71d06fae1
Kiran Billur <kiranbillur7760@gmail.com>
2026-08-07 14:03:41 +0530

    Add collaboration notification type

--- server/server/models/Notification.js
+++ server/server/models/Notification.js
@@ -19,6 +19,7 @@ const notificationSchema = new mongoose.Schema(
         "like",
         "comment",
         "project_invite",
+        "collaboration",
         "task_assigned",
         "project_update",
       ],
